32b1c87575f82537b881a5e2ed59e0ab3edddcff
martin sisk <martin.sisk@pop.belmont.edu>
2020-04-28 11:29:40 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+// exercise 1
+/*  Take the following data and represent it in
+
+A string
+An object
+An array
+"lunch"
+"peanut butter and jelly"
+"whole wheat"
+"grape juice"
+"apple"
+ */
+
+let lunchBox = "lunchMeal, lunch, peanut butter and jelly, whole wheat, grape juice, apple"
+console.log(lunchBox);
+
+var lunchBox2 = {
+    meal: 'lunch',
+    sandwich: 'peanut butter and jelly',
+    bread: 'whole wheat',
+    drink: 'grape juice',
+    fruit: 'apple'
+};
+console.log(lunchBox2);
+
+const lunchBox3 = ['lunch', 'peanut butter and jelly', 'whole wheat', 'grape juice', 'apple'];
+const snack = lunchBox3.push('chips');
+console.log(lunchBox3);
+
